594f651d20e4454afc1b56332d49963548a00eda
OneTrickDragon7 <69195984+OneTrickDragon@users.noreply.github.com>
2026-03-22 20:32:34 +0530

    Attn

--- Residuals.py
+++ Residuals.py
@@ -8,3 +8,14 @@ class RMSNorm(nn.Module):
 
     def forward(self, x:torch.Tensor) -> torch.Tensor:
           return x * torch.rsqrt(x.pow(2).mean(dim=-1, keepdim=True) + self.eps)
+
+class FullAttnRes(nn.Module):
+    def __init__(self, num_layers: int, d_model: int):
+        super().__init__()
+        self.num_layers = num_layers
+        self.d_model = d_model
+        self.w = nn.Parameter(torch.zeros(num_layers, d_model))
+        self.norm = RMSNorm(d_model)
+
+    
+          
